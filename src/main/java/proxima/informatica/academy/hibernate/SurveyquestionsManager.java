bf84c5ab4fd580b31f38159d6806cf774e1fafd7
Giacomo Della Luna <giacomo.dellaluna@gmail.com>
2023-01-25 16:45:50 +0100

    crud methods Surveyquestions with tests

--- src/main/java/proxima/informatica/academy/hibernate/SurveyquestionsManager.java
+++ src/main/java/proxima/informatica/academy/hibernate/SurveyquestionsManager.java
@@ -1,6 +1,10 @@
 package proxima.informatica.academy.hibernate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,7 +16,7 @@ public class SurveyquestionsManager {
 	private final static Logger logger = LoggerFactory.getLogger(SurveyquestionsManager.class);
 	
 	public static int insert (SurveyquestionsDto item) {
-		logger.debug("RoleManager.insert - START - item: " + item);
+		logger.debug("SurveyquestionsManager.insert - START - item: " + item);
 		int id_inserted_value = 0 ;
 		try {
 			Session session = DBManager.getSessionFactory().openSession();
@@ -24,8 +28,74 @@ public class SurveyquestionsManager {
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 		}
-		logger.debug("RoleManager.insert - END - id_inserted_value: " + id_inserted_value);
+		logger.debug("SurveyquestionsManager.insert - END - id_inserted_value: " + id_inserted_value);
         return id_inserted_value ;
+	}
+	
+	public static boolean deleteAll () {
+		logger.debug("SurveyquestionsManager.deleteAll - START");
+		boolean returnFalse = false ;
+		try {
+			Session session = DBManager.getSessionFactory().openSession();
+			session.beginTransaction();
+		    Query<SurveyquestionsDto> query = session.createQuery("delete from " + SurveyquestionsDto.class.getSimpleName());
+		    query.executeUpdate();
+			session.getTransaction().commit();
+			session.close();	
+			returnFalse = true ;
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			returnFalse = false ;
+		}
+		logger.debug("SurveyquestionsManager.deleteAll - END");   
+		return returnFalse ;
+	}
+	
+	public static void delete(SurveyquestionsDto item) {
+		logger.debug("SurveyquestionsManager.delete - START - item: " + item);
+		try {
+			Session session = DBManager.getSessionFactory().openSession();
+			session.beginTransaction();
+			session.delete(item);
+			session.getTransaction().commit();
+			session.close();
+		} catch(Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		logger.debug("SurveyquestionsManager.delete - END");
+	}
+	
+	public static SurveyquestionsDto selectById (int id) {
+		logger.debug("SurveyquestionsManager.selectById - START - id: " + id);
+		SurveyquestionsDto item = null ;
+		try {
+			Session session = DBManager.getSessionFactory().openSession();
+			session.beginTransaction();
+            item = session.get(SurveyquestionsDto.class, id);
+			session.getTransaction().commit();
+			session.close();			
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		logger.debug("SurveyquestionsManager.selectById - END - item: " + item);
+		return item ;
+	}
+	
+	public static List<SurveyquestionsDto> selectAll () {
+		logger.debug("SurveyquestionsManager.selectAll - START");
+		List<SurveyquestionsDto> list = new ArrayList<SurveyquestionsDto> () ;
+		try {
+			Session session = DBManager.getSessionFactory().openSession();
+//			session.beginTransaction();
+			Query<SurveyquestionsDto> query = session.createQuery("SELECT qst FROM " + SurveyquestionsDto.class.getSimpleName() + " qst");
+			list = query.getResultList();
+//			session.getTransaction().commit();
+			session.close();			
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		logger.debug("SurveyquestionsManager.selectAll - END - items.size(): " + list.size());
+		return list ;
 	}
 
 }
--- src/test/java/proxima/informatica/academy/hibernate/SurveyquestionsManagerTest.java
+++ src/test/java/proxima/informatica/academy/hibernate/SurveyquestionsManagerTest.java
@@ -2,6 +2,8 @@ package proxima.informatica.academy.hibernate;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,18 +12,72 @@ import proxima.informatica.academy.dto.SurveyquestionsDto;
 
 public class SurveyquestionsManagerTest {
 	private final static Logger logger = LoggerFactory.getLogger(SurveyquestionsManagerTest.class);
-	
+
 	@Test
-	public void insertTest() {
+	public void deleteAllOK() {
 		System.out.println("#########");
-		System.out.println("TEST INSERT");
+		System.out.println("TEST DELETE ALL");
 		System.out.println("#########");
+		SurveyquestionsManager.deleteAll();
 		SurveyquestionsDto sq = new SurveyquestionsDto();
 		sq.setQuestionId(1);
 		sq.setSurveyId(1);
-    	
-    	int id_inserted_value = SurveyquestionsManager.insert(sq);
+		SurveyquestionsManager.insert(sq);
+		assertTrue(SurveyquestionsManager.deleteAll());
+	}
+
+	@Test
+	public void insertOK() {
+		System.out.println("#########");
+		System.out.println("TEST INSERT");
+		System.out.println("#########");
+		SurveyquestionsManager.deleteAll();
+		SurveyquestionsDto sq = new SurveyquestionsDto();
+		sq.setQuestionId(1);
+		sq.setSurveyId(1);
+
+		int id_inserted_value = SurveyquestionsManager.insert(sq);
 		logger.debug("@Test --> inserted: " + id_inserted_value);
-        assertTrue( id_inserted_value>0 );
+		assertTrue(id_inserted_value > 0);
+	}
+
+	@Test
+	public void selectAllOK() {
+		System.out.println("#########");
+		System.out.println("TEST SELECT ALL");
+		System.out.println("#########");
+		SurveyquestionsManager.deleteAll();
+		SurveyquestionsDto sq = new SurveyquestionsDto();
+		sq.setQuestionId(1);
+		sq.setSurveyId(1);
+		SurveyquestionsManager.insert(sq);
+		assertTrue(SurveyquestionsManager.selectAll().size() > 0);
+	}
+
+	@Test
+	public void selectByIdOK() {
+		System.out.println("#########");
+		System.out.println("TEST SELECT BY ID");
+		System.out.println("#########");
+		SurveyquestionsManager.deleteAll();
+		SurveyquestionsDto sq = new SurveyquestionsDto();
+		sq.setQuestionId(1);
+		sq.setSurveyId(1);
+		SurveyquestionsManager.insert(sq);
+		List<SurveyquestionsDto> list = SurveyquestionsManager.selectAll();
+		assertTrue(SurveyquestionsManager.selectById(list.get(0).getId()) != null);
+	}
+
+	@Test
+	public void deleteOK() {
+		System.out.println("#########");
+		System.out.println("TEST DELETE");
+		System.out.println("#########");
+		SurveyquestionsManager.deleteAll();
+		SurveyquestionsDto sq = new SurveyquestionsDto();
+		sq.setQuestionId(1);
+		sq.setSurveyId(1);
+		SurveyquestionsManager.delete(sq);
+		assertTrue(SurveyquestionsManager.selectAll().size() == 0);
 	}
 }

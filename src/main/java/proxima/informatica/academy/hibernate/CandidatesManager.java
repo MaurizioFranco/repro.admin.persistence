0926bbf1369ba0c2160f20c4e39f3c3da8e76a31
Antonio <Antonio@LAPTOP-GPM0CA74>
2023-01-25 16:48:16 +0100

    CRUD methods added

--- src/main/java/proxima/informatica/academy/hibernate/CandidatesManager.java
+++ src/main/java/proxima/informatica/academy/hibernate/CandidatesManager.java
@@ -3,8 +3,11 @@
  */
 package proxima.informatica.academy.hibernate;
 
-import org.hibernate.Session;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,6 +39,72 @@ public class CandidatesManager {
 		}
 		logger.debug("CandidatesManager.insert - END - id_inserted_value: " + id_inserted_value);
         return id_inserted_value ;
+	}
+	
+	public static void delete (CandidatesDto item) {
+		logger.debug("CandidatesManager.delete - START - item: " + item);
+		try {
+			Session session = DBManager.getSessionFactory().openSession();
+			session.beginTransaction();
+			session.delete(item);			
+			session.getTransaction().commit();
+			session.close();			
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		logger.debug("CandidatesManager.delete - END");        
+	}
+	
+	public static CandidatesDto selectById (int id) {
+		logger.debug("CandidatesManager.selectById - START - id: " + id);
+		CandidatesDto item = null ;
+		try {
+			Session session = DBManager.getSessionFactory().openSession();
+			session.beginTransaction();
+            item = session.get(CandidatesDto.class, id);
+			session.getTransaction().commit();
+			session.close();			
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		logger.debug("CandidatesManager.selectById - END - item: " + item);
+		return item ;
+	}
+	
+	public static boolean deleteAll () {
+		logger.debug("CandidatesManager.deleteAll - START");
+		boolean returnFalse = false ;
+		try {
+			Session session = DBManager.getSessionFactory().openSession();
+			session.beginTransaction();
+		    Query<CandidatesDto> query = session.createQuery("delete from " + CandidatesDto.class.getSimpleName());
+		    query.executeUpdate();
+			session.getTransaction().commit();
+			session.close();	
+			returnFalse = true ;
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			returnFalse = false ;
+		}
+		logger.debug("CandidatesManager.deleteAll - END");   
+		return returnFalse ;
+	}
+	
+	public static List<CandidatesDto> selectAll () {
+		logger.debug("CandidatesManager.selectAll - START");
+		List<CandidatesDto> list = new ArrayList<CandidatesDto> () ;
+		try {
+			Session session = DBManager.getSessionFactory().openSession();
+			session.beginTransaction();
+			Query<CandidatesDto> query = session.createQuery("SELECT qst FROM " + CandidatesDto.class.getSimpleName() + " qst");
+			list = query.getResultList();
+			session.getTransaction().commit();
+			session.close();			
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		logger.debug("CandidatesManager.selectAll - END - items.size(): " + list.size());
+		return list ;
 	}
 
 }

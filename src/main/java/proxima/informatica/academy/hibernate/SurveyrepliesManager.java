2dda9a89c2e54ebbc632424b6b73aeef822ffba3
Giammarco93 <giammarco.lucchetti@hotmail.it>
2023-01-25 16:48:52 +0100

    Added method for delete insert and select and relative test,update static method for user insert

--- src/main/java/proxima/informatica/academy/hibernate/SurveyrepliesManager.java
+++ src/main/java/proxima/informatica/academy/hibernate/SurveyrepliesManager.java
@@ -3,10 +3,15 @@
  */
 package proxima.informatica.academy.hibernate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import proxima.informatica.academy.dto.RoleDto;
 import proxima.informatica.academy.dto.SurveyrepliesDto;
 import proxima.informatica.academy.dto.UserDto;
 
@@ -21,7 +26,7 @@ public class SurveyrepliesManager {
 	
 	private final static Logger logger = LoggerFactory.getLogger(SurveyrepliesManager.class);
 	
-	public int insert (SurveyrepliesDto item) {
+	public static int insert (SurveyrepliesDto item) {
 		logger.debug("SurveyrepliesDto.insert - START - item: " + item);
 		int id_inserted_value = 0 ;
 		try {
@@ -36,6 +41,68 @@ public class SurveyrepliesManager {
 		}
 		logger.debug("SurveyrepliesDto.insert - END - id_inserted_value: " + id_inserted_value);
         return id_inserted_value ;
+	}
+	
+	public static void delete (SurveyrepliesDto item) {
+		logger.debug("RoleManager.delete - START - item: " + item);
+		try {
+			Session session = DBManager.getSessionFactory().openSession();
+			session.beginTransaction();
+			session.delete(item);			
+			session.getTransaction().commit();
+			session.close();			
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		logger.debug("SurveyrepliesDto.delete - END");        
+	}
+	
+	public static SurveyrepliesDto selectById (int id) {
+		logger.debug("SurveyrepliesDto.selectById - START - id: " + id);
+		SurveyrepliesDto item = null ;
+		try {
+			Session session = DBManager.getSessionFactory().openSession();
+            item = session.get(SurveyrepliesDto.class, id);
+			session.close();			
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		logger.debug("SurveyrepliesDto.selectById - END - item: " + item);
+		return item ;
+	}
+	
+	public static boolean deleteAll () {
+		logger.debug("SurveyrepliesDto.deleteAll - START");
+		boolean returnFalse = false ;
+		try {
+			Session session = DBManager.getSessionFactory().openSession();
+			session.beginTransaction();
+		    Query<SurveyrepliesDto> query = session.createQuery("delete from " + SurveyrepliesDto.class.getSimpleName());
+		    query.executeUpdate();
+			session.getTransaction().commit();
+			session.close();	
+			returnFalse = true ;
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			returnFalse = false ;
+		}
+		logger.debug("SurveyrepliesDto.deleteAll - END");   
+		return returnFalse ;
+	}
+	
+	public static List<SurveyrepliesDto> selectAll () {
+		logger.debug("SurveyrepliesDto.selectAll - START");
+		List<SurveyrepliesDto> list = new ArrayList<SurveyrepliesDto> () ;
+		try {
+			Session session = DBManager.getSessionFactory().openSession();
+			Query<SurveyrepliesDto> query = session.createQuery("SELECT qst FROM " + SurveyrepliesDto.class.getSimpleName() + " qst");
+			list = query.getResultList();
+			session.close();			
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		logger.debug("SurveyrepliesDto.selectAll - END - items.size(): " + list.size());
+		return list ;
 	}
 
 }
--- src/test/java/proxima/informatica/academy/hibernate/SurveyrepliesDBManagerTest.java
+++ src/test/java/proxima/informatica/academy/hibernate/SurveyrepliesDBManagerTest.java
@@ -16,6 +16,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import proxima.informatica.academy.dto.RoleDto;
+import proxima.informatica.academy.dto.SurveyDto;
 import proxima.informatica.academy.dto.SurveyrepliesDto;
 import proxima.informatica.academy.dto.UserDto;
 
@@ -32,26 +34,130 @@ public class SurveyrepliesDBManagerTest {
 		System.out.println("#########");
 		System.out.println("TEST INSERT");
 		System.out.println("#########");
-		SurveyrepliesDto survey = new SurveyrepliesDto();
-		BigInteger survey_id = new BigInteger("1");
+		
+		assertTrue(SurveyrepliesManager.deleteAll());
+		assertTrue(SurveyManager.deleteAll());
+		assertTrue(UserManager.deleteAll());
+		
+		SurveyrepliesDto surveyreplies = insertAllTables();
+    	Session session = DBManager.getSessionFactory().openSession();
+        session.beginTransaction();
+        int id_inserted_value_surveyreplies = SurveyrepliesManager.insert(surveyreplies);
+        session.getTransaction().commit();
+        session.close();
+		logger.debug("@Test --> inserted: " + id_inserted_value_surveyreplies);
+        assertTrue( id_inserted_value_surveyreplies >0 );
+	}
+	
+	@Test
+	public void testDeleteAll() {
+		System.out.println("#########");
+		System.out.println("TEST DELETE ALL - START");
+		System.out.println("#########");
+		
+		SurveyrepliesDto surveyreplies = insertAllTables();
+    	Session session = DBManager.getSessionFactory().openSession();
+        int id_inserted_value_surveyreplies = SurveyrepliesManager.insert(surveyreplies);
+		logger.debug("@Test --> inserted: " + id_inserted_value_surveyreplies);
+        assertTrue( id_inserted_value_surveyreplies >0 );
+			
+		boolean returnValue = SurveyrepliesManager.deleteAll();
+		session.beginTransaction();
+		session.getTransaction().commit();
+		session.close();
+		logger.debug("@Test --> returnValue: " + returnValue);
+        assertTrue( returnValue );
+        System.out.println("#########");
+		System.out.println("TEST DELETE ALL - END");
+		System.out.println("#########");
+	}
+	
+	@Test
+	public void testSelectAllEmpty() {
+		System.out.println("#########");
+		System.out.println("TEST SELECT ALL EMPTY- START");
+		System.out.println("#########");
+		
+		SurveyrepliesDto surveyreplies = new SurveyrepliesDto();
+		BigInteger surveyreplies_id = new BigInteger("5");
+		BigInteger userreplies_id = new BigInteger("37");
+		surveyreplies.setSurvey_id(surveyreplies_id);
+		surveyreplies.setUser_id(userreplies_id);
+		Session session = DBManager.getSessionFactory().openSession();
+        int id_inserted_value_surveyreplies = SurveyrepliesManager.insert(surveyreplies);
+        assertTrue( id_inserted_value_surveyreplies >0 );
+		
+        boolean returnValue = SurveyrepliesManager.deleteAll();
+        logger.debug("@Test --> returnValue: " + returnValue);
+        assertTrue( returnValue );
+        session.beginTransaction();
+        session.getTransaction().commit();
+        session.close();
+        int selectAllSize = SurveyrepliesManager.selectAll().size();
+		logger.debug("@Test --> selectAllSize: " + selectAllSize);
+        assertTrue( selectAllSize==0 );
+        System.out.println("#########");
+        System.out.println("TEST SELECT ALL EMPTY- END");
+		System.out.println("#########");
+	}
+	
+	@Test
+	public void testSelectAllForOne() {
+		System.out.println("#########");
+		System.out.println("TEST SELECT ALL FOR ONE - START");
+		System.out.println("#########");
+		boolean returnValue = SurveyrepliesManager.deleteAll();
+		logger.debug("@Test --> returnValue: " + returnValue);
+        assertTrue( returnValue );
+        int selectAllSize = SurveyrepliesManager.selectAll().size();
+		logger.debug("@Test --> selectAllSize: " + selectAllSize);
+        assertTrue( selectAllSize==0 );
+        SurveyrepliesDto survey = new SurveyrepliesDto();
+        BigInteger survey_id = new BigInteger("1");
 		BigInteger user_id = new BigInteger("33");
 		survey.setSurvey_id(survey_id);
 		survey.setUser_id(user_id);
-//		survey.setStarttime(null);
-//		survey.setEndtime(null);
-//		survey.setAnswers(null);
-//		survey.setPdffilename(null);
-//		survey.setPoints(null);
-    	Session session = DBManager.getSessionFactory().openSession();
-        session.beginTransaction();
-        Object generatedIdentifier = session.save(survey);
-//        logger.debug("insertNewUser - DEBUG - generatedIdentifier" + ((Integer)generatedIdentifier).intValue());
-        int id_inserted_value = ((Integer)generatedIdentifier).intValue();
-        session.getTransaction().commit();
-        session.close();
-    	
+    	int id_inserted_value = SurveyrepliesManager.insert(survey);
 		logger.debug("@Test --> inserted: " + id_inserted_value);
-        assertTrue( id_inserted_value>0 );
+        assertTrue( id_inserted_value>0 );        
+        selectAllSize = SurveyrepliesManager.selectAll().size();
+		logger.debug("@Test --> selectAllSize: " + selectAllSize);
+        assertTrue( selectAllSize==1 );
+        System.out.println("#########");
+        System.out.println("TEST SELECT ALL FOR ONE - END");
+		System.out.println("#########");
 	}
 	
+	
+	
+	public SurveyrepliesDto insertAllTables() {
+		SurveyDto survey = new SurveyDto();
+		survey.setLabel("testLabel");
+		survey.setTime(1l);
+		survey.setDescription("TEST DESCRIPTION");
+		int id_inserted_value_survey = SurveyManager.insert(survey);
+		assertTrue( id_inserted_value_survey >0 );
+		
+		UserDto user = new UserDto();
+    	user.setEmail("giammarclucchetti@hotmail.it");
+    	user.setPassword("ciao");
+    	user.setFirstname("Giammarco");
+    	user.setLastname("Lucchetti");
+    	user.setEnabled(false);
+    	user.setRole(0);
+    	user.setDateofbirth(Date.valueOf(LocalDate.now()));
+    	user.setRegdate(Timestamp.valueOf(LocalDateTime.now()));
+    	user.setRole(10);
+    	int id_inserted_value_user = UserManager.insert(user);
+    	assertTrue( id_inserted_value_user >0 );
+		
+		SurveyrepliesDto surveyreplies = new SurveyrepliesDto();
+		String survey_id = String.valueOf(survey.getId());
+		String user_id = String.valueOf(user.getId());
+		BigInteger surveyreplies_id = new BigInteger(survey_id);
+		BigInteger userreplies_id = new BigInteger(user_id);
+		surveyreplies.setSurvey_id(surveyreplies_id);
+		surveyreplies.setUser_id(userreplies_id);
+		return surveyreplies;
+	}
 }

1a293e9c8c576d5c8ab4dd8b788aa1c6b57c86e8
DaimCod <daim.kazmi00@gmail.com>
2023-01-25 16:42:30 +0100

    CRUD methods finished for QuestionsDto

--- src/main/java/proxima/informatica/academy/hibernate/QuestionsManager.java
+++ src/main/java/proxima/informatica/academy/hibernate/QuestionsManager.java
@@ -1,11 +1,15 @@
 package proxima.informatica.academy.hibernate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import proxima.informatica.academy.dto.QuestionsDto;
+import proxima.informatica.academy.dto.RoleDto;
 
 public class QuestionsManager {
 
@@ -36,9 +40,44 @@ public class QuestionsManager {
 		return id_inserted_value;
 	}
 	
+	public static QuestionsDto selectById(int id) {
+		logger.debug("QuestionsManager.delete - START - id: " + id);
+		QuestionsDto item = null ;
+		try {
+			Session session = DBManager.getSessionFactory().openSession();
+//			session.beginTransaction();
+            item = session.get(QuestionsDto.class, id);
+//			session.getTransaction().commit();
+			session.close();			
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		logger.debug("QuestionsDto.selectById - END - item: " + item);
+		return item ;
+	}
+	
+	public static boolean deleteById(int id) {
+		logger.debug("QuestionsDto.delete - START - id: " + id);
+		boolean val = false;
+		QuestionsDto question = null;
+		try {
+			question = QuestionsManager.selectById(id);
+			Session session = DBManager.getSessionFactory().openSession();
+			session.beginTransaction();
+			session.delete(question);			
+			session.getTransaction().commit();
+			session.close();	
+			val = true;
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		logger.debug("QuestionsDto.delete - END");        
+		return val;
+	}
+	
 	public static boolean deleteAll () {
 		logger.debug("RoleManager.deleteAll - START");
-		boolean returnFalse = false ;
+		boolean returnValue = false ;
 		try {
 			Session session = DBManager.getSessionFactory().openSession();
 			session.beginTransaction();
@@ -46,12 +85,29 @@ public class QuestionsManager {
 		    query.executeUpdate();
 			session.getTransaction().commit();
 			session.close();	
-			returnFalse = true ;
+			returnValue = true ;
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
-			returnFalse = false ;
+			returnValue = false ;
 		}
 		logger.debug("RoleManager.deleteAll - END");   
-		return returnFalse ;
+		return returnValue ;
+	}
+	
+	public static List<QuestionsDto> selectAll () {
+		logger.debug("QuestionsDto.selectAll - START");
+		List<QuestionsDto> list = new ArrayList<QuestionsDto> () ;
+		try {
+			Session session = DBManager.getSessionFactory().openSession();
+//			session.beginTransaction();
+			Query<QuestionsDto> query = session.createQuery("SELECT qst FROM " + QuestionsDto.class.getSimpleName() + " qst");
+			list = query.getResultList();
+//			session.getTransaction().commit();
+			session.close();			
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		logger.debug("QuestionsDto.selectAll - END - items.size(): " + list.size());
+		return list ;
 	}
 }
--- src/test/java/proxima/informatica/academy/hibernate/QuestionsDBManagerTest.java
+++ src/test/java/proxima/informatica/academy/hibernate/QuestionsDBManagerTest.java
@@ -6,13 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import proxima.informatica.academy.dto.QuestionsDto;
+import proxima.informatica.academy.dto.RoleDto;
 
 public class QuestionsDBManagerTest {
 
 	private final static Logger logger = LoggerFactory.getLogger(QuestionsDBManagerTest.class);
 	
 	@Test
-	public void test2() {
+	public void insertQuestion() {
 		System.out.println("#########");
 		System.out.println("TEST INSERT");
 		System.out.println("#########");
@@ -32,5 +33,88 @@ public class QuestionsDBManagerTest {
     	int id_inserted_value = QuestionsManager.insert(question);
 		logger.debug("@Test --> inserted: " + id_inserted_value);
         assertTrue( id_inserted_value>0 );
+	}
+	
+	@Test
+	public void selectById() {
+		System.out.println("#########");
+		System.out.println("TEST SELECT BY ID");
+		System.out.println("#########");
+		QuestionsDto question = new QuestionsDto();
+		question.setLabel("test label");
+		int idToFind = QuestionsManager.insert(question);
+		logger.debug("@Test --> idAdded: " + idToFind);
+		assertTrue( idToFind == QuestionsManager.selectById(idToFind).getId());
+	}
+	
+	
+	@Test
+	public void deleteById() {
+		System.out.println("#########");
+		System.out.println("TEST delete BY ID");
+		System.out.println("#########");
+		QuestionsDto question = new QuestionsDto();
+		question.setLabel("test label");
+		int idToDelete = QuestionsManager.insert(question);
+		System.out.println("Id da eliminare: " + idToDelete);
+		logger.debug("@Test --> idAdded: " + idToDelete);
+		assertTrue(QuestionsManager.deleteById(idToDelete));
+	}
+	
+	
+	@Test
+	public void deleteAll() {
+		System.out.println("#########");
+		System.out.println("TEST DELETE ALL");
+		System.out.println("#########");
+		boolean value = QuestionsManager.deleteAll();
+		logger.debug("@Test --> value: " + value);
+		assertTrue( value );
+        System.out.println("#########");
+		System.out.println("TEST DELETE ALL - END");
+		System.out.println("#########");
+	}
+	
+	
+	@Test
+	public void selectAllEmpty() {
+		System.out.println("#########");
+		System.out.println("TEST SELECT ALL EMPTY- START");
+		System.out.println("#########");
+		boolean returnValue = QuestionsManager.deleteAll();
+		logger.debug("@Test --> returnValue: " + returnValue);
+        assertTrue( returnValue );
+        int selectAllSize = QuestionsManager.selectAll().size();
+		logger.debug("@Test --> selectAllSize: " + selectAllSize);
+        assertTrue( selectAllSize==0 );
+        System.out.println("#########");
+        System.out.println("TEST SELECT ALL EMPTY- END");
+		System.out.println("#########");
+	}
+	
+	
+	
+	@Test
+	public void selectAllForOne() {
+		System.out.println("#########");
+		System.out.println("TEST SELECT ALL FOR ONE - START");
+		System.out.println("#########");
+		boolean returnValue = QuestionsManager.deleteAll();
+		logger.debug("@Test --> returnValue: " + returnValue);
+        assertTrue( returnValue );
+        int selectAllSize = QuestionsManager.selectAll().size();
+		logger.debug("@Test --> selectAllSize: " + selectAllSize);
+        assertTrue( selectAllSize==0 );
+        QuestionsDto question = new QuestionsDto();
+        question.setLabel("test label");
+    	int id_inserted_value = QuestionsManager.insert(question);
+		logger.debug("@Test --> inserted: " + id_inserted_value);
+        assertTrue( id_inserted_value>0 );        
+        selectAllSize = QuestionsManager.selectAll().size();
+		logger.debug("@Test --> selectAllSize: " + selectAllSize);
+        assertTrue( selectAllSize==1 );
+        System.out.println("#########");
+        System.out.println("TEST SELECT ALL FOR ONE - END");
+		System.out.println("#########");
 	}
 }

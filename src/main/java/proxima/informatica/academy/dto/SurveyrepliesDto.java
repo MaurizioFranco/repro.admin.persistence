955db9ee5504d6c327c1cf927f30f9954f3669d6
Giammarco93 <giammarco.lucchetti@hotmail.it>
2023-01-26 14:53:54 +0100

    Update test and set mapping xml

--- src/main/java/proxima/informatica/academy/dto/SurveyrepliesDto.java
+++ src/main/java/proxima/informatica/academy/dto/SurveyrepliesDto.java
@@ -5,12 +5,23 @@ import java.sql.Date;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 
 /**
  * @author Giammarco Lucchetti
  */
+
+@Entity
+@Table(name = "surveyreplies")
 public class SurveyrepliesDto {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private BigInteger survey_id;
 	private BigInteger user_id;
